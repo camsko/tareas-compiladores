da1f7d2b7ef6e782c8953eb6808bbb642fd626ed
dannycg25 <dannycg25@gmail.com>
2026-05-27 18:50:49 -0600

    Some regex and error handling fixes

---
+++ proyecto/proyecto_2/tests/test3.py
@@ -0,0 +1,18 @@
+if (a):
+    if (b):
+        if (False):
+            if (d):
+                pass
+                
+    if (e):
+        pass
+        print()
+        pass
+    pass
+    print(a)
+print("help")
+
+if (False):
+    if (True):
+        pass
+        a = b
