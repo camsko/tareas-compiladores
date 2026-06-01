6e0978cc9a657d6fedb67fba88f6e47fac56d443
Camila <camfo@hotmail.com>
2026-06-01 00:54:43 -0600

    functions for member access such as self.func()

--- proyecto/proyecto_2/ExpressionRules.py
+++ proyecto/proyecto_2/ExpressionRules.py
@@ -16,9 +16,43 @@ class ExpressionRules:
     def p_num_expression(self, p):
         'expression : num_expression'
         p[0] = p[1]
+
+    def p_identifier_member_access_expression(self, p):
+        'expression : ID member_access'
+        p[0] = MemberAccessNode(
+            IdentifierNode(p[1]),
+            p[2]
+        )    
+    def p_function_call_member_access_expression(self, p):
+        'expression : function_call member_access'
+        p[0] = MemberAccessNode(
+            p[1],
+            p[2]
+        )
     
+    def p_member_access_multiple(self, p):
+        'member_access : member_access DOT ID'
+        p[0] = p[1] + [IdentifierNode(p[3])]
+
+    def p_member_access_single(self, p):
+        'member_access : DOT ID'
+        p[0] = [IdentifierNode(p[2])]
+
+    def p_member_access_function_multiple(self, p):
+        'member_access : member_access DOT function_call'
+        p[0] = p[1] + [p[3]]
+
+    def p_member_access_function_single(self, p):
+        'member_access : DOT function_call'
+        p[0] = [p[2]]
+
     def p_identifier_expression(self, p):
         'expression : ID'
         p[0] = IdentifierNode(p[1])
-    
+
+    #TODO CHECK IF THIS IS NEEDED, lo agregue para que se puedan usar funciones luego de asignaciones. 
+    #Sin embargo, también permite que se usen funciones como argumentos
+    def p_function_call_expression(self, p):
+        'expression : function_call'
+        p[0] = p[1]
     
